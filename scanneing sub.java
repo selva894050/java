b539f831244387353a19ec5397a93f90881c7efa
selva894050 <43056945+selva894050@users.noreply.github.com>
2018-09-07 15:13:08 +0530

    Add files via upload

---
+++ scanneing sub.java
@@ -0,0 +1,11 @@
+import java.util.*;
+  class Main {
+  public static void main(String[] args) { 
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the a value");
+    float a=sc.nextInt();
+    float b=5;
+    float c=a/b;
+    System.out.println("sub:"+c);
+  }
+}
