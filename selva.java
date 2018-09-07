6063ce889fe602cdc4203afb5e0d70b2bfe2a05f
selva894050 <43056945+selva894050@users.noreply.github.com>
2018-09-07 11:50:08 +0530

    Add files via upload

---
+++ selva.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    float a,b,c,d,e,f;
+    a=22;
+    b=33;
+    c=a+b;
+    d=a/b;
+    e=a*b;
+    f=a-b;
+    System.out.println("adding value:"+c);
+    System.out.println("divided value:"+d);
+    System.out.println("multiplied value:"+e);
+    System.out.println("sub value:"+f);
+  }
+}
